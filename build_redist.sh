f6271374bbb0d19bdad07871e8a9c7df6092109c
Carlos Panato <ctadeu@gmail.com>
2021-09-19 16:01:28 +0200

    debugging

--- build_redist.sh
+++ build_redist.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+: "${REPO_ONWER:?Repository Owner not set.}"
+
 export eTAG="latest-dev"
 echo $1
 if [ $1 ] ; then
   eTAG=$1
 fi
 
-docker create --name faas-cli openfaas/faas-cli:${eTAG} && \
+docker create --name faas-cli ghcr.io/openfaas/faas-cli:${eTAG} && \
 mkdir -p ./bin && \
 docker cp faas-cli:/home/app/faas-cli ./bin && \
 docker cp faas-cli:/home/app/faas-cli-darwin ./bin && \
